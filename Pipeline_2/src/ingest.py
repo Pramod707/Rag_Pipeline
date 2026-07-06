b9963a052b8d9b838c81598d646aadf61cdbe65b
Pramod <pramod4342@gmail.com>
2026-07-06 21:31:55 +0530

    Refactor code structure for improved readability and maintainability

--- Pipeline_2/src/ingest.py
+++ Pipeline_2/src/ingest.py
@@ -1,15 +1,27 @@
-from langchain_community.documents_loaders import DirectoryLoader, TextLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+# from langchain_community.document_loaders import DirectoryLoader, TextLoader
+# from langchain_community.document_loaders import DirectoryLoader, TextLoader
+from langchain_community.document_loaders import DirectoryLoader, TextLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_ollama import OllamaEmbeddings
-from langchain_community.vectorstore import Chroma
+from langchain_community.vectorstores import Chroma
 
 
 ##load file
 
 loader = DirectoryLoader(
-    path="/docs",
+    path="../docs2",
     glob="*.txt",
     loader_cls=TextLoader,
     loader_kwargs={"encoding": "utf-8"},
 )
 documents = loader.load()
+
+print(f"Loaded {len(documents)} documents\n")
+
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=500,
+    chunk_overlap=100,
+)
+
+chunks = text_splitter.split_documents(documents)
+print(f"total chunls {len(chunks)}")
